48234ca5d80b081758240ae0ee5a5199dd1c3937
lijiangzxc <lijiang824@163.com>
2016-06-28 17:30:56 +0800

    初次更新

--- src/main/java/com/likou/dxf4j/io/DXFFileInputStream.java
+++ src/main/java/com/likou/dxf4j/io/DXFFileInputStream.java
@@ -1,7 +1,25 @@
 package com.likou.dxf4j.io;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by jiangli on 16/6/23.
  */
 public class DXFFileInputStream {
+    public static void main(String[] args) throws IOException
+    {
+        method("/Users/jiangli/Downloads/金港国际2013.dxf");//传入要读取的文件，字符串型。
+    }
+    public static void method(String s) throws IOException
+    {
+        FileReader f=new FileReader(s);
+        char[] b=new char[1024];
+        int num=0;
+        while((num=f.read(b))!=-1)
+        {
+            System.out.println(new String(b,0,num));
+        }
+        f.close();
+    }
 }
